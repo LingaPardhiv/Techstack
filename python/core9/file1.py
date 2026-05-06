d2b90bd1c9bda112ee9f5eb505757c4f7613dc05
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-05-06 22:21:44 +0530

    changed prime Numbers

--- python/core9/file1.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-l=[5, 10, 15, 20, 25, 30]
-sq=list(map(lambda x:x**2,l))
-di = list(filter(lambda x:x%5 == 0,sq))
-v = reduce(lambda x,y: x+y,di)
-v1= reduce(lambda x,y: x+y,filter(lambda x:x%5 == 0,map(lambda x:x**2,l)))
-print(v,v1)
