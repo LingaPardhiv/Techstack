784d08e9bfe72538d44ecae248aa01f7326550f9
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-05-07 11:39:23 +0530

    Changed prime Numbers file

--- python/programming/Prime Numbers.py
+++ python/programming/Prime Numbers.py
@@ -1,18 +1,18 @@
 #Alternative Prime Numbers in a given Range
-a=int(input())
-b=int(input())
-c=0
-for i in range(a,b+1):
-    fc=0
-    for j in range(1,i+1):
-        if i%j==0:
-            fc=fc+1
-    if fc==2:
-        c+=1
-        if c%2==1:
-            if c!=1:
-                print(",",end="")
-            print(i,end="")
+# a=int(input())
+# b=int(input())
+# c=0
+# for i in range(a,b+1):
+#     fc=0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#             fc=fc+1
+#     if fc==2:
+#         c+=1
+#         if c%2==1:
+#             if c!=1:
+#                 print(",",end="")
+#             print(i,end="")
 
 #if a number is prime number print its position if not print not a prime number
 def isPrime(n):
