6537e98c59b3d2350f5362aab2d5bd8887ae94dc
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-05-18 15:49:27 +0530

    Added code

--- python/core9/MagicMethodsQs.py
+++ python/core9/MagicMethodsQs.py
@@ -44,3 +44,50 @@
 #
 # acc1.deposit(2000)
 # acc2.withdraw(1000)
+
+#Q2
+class Product:
+    def __init__(self,name,price,quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+    def total_price(self):
+        return self.price*self.quantity
+
+    def __str__(self):
+        return f'Product {self.name} , price {self.price} , quantity {self.quantity}'
+
+    def __add__(self,other):
+        return self.total_price() + other.total_price()
+
+    def __mul__(self,number):
+        return self.price*number
+
+    def __gt__(self,other):
+        return self.total_price() > other.total_price()
+
+    def __eq__(self,other):
+        return self.price == other.price
+
+    def __getattr__(self,attr):
+        return "Attribute not found"
+
+    def __setattr__(self,key,value):
+        if key=="price" and value<0:
+            print("price value cannot be negative")
+        else:
+            super().__setattr__(key,value)
+
+p1 = Product("Laptop", 50000, 2)
+p2 = Product("Phone", 30000, 3)
+
+print(p1)
+print("Total Price of p1:", p1.total_price())
+print("Combined Total:", p1 + p2)
+print("Price multiplied:", p1 * 2)
+print("p1 has greater value:", p1 > p2)
+print("Prices are equal:", p1 == p2)
+print(p1.color)
+p1.price = -1000
+print(p1.price)
