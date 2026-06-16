eb5c1ee38dbf504301bae2b88b5a7a452cbf51d4
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-06-16 22:42:21 +0530

    feat: add solutions to List questions

---
+++ python/programming/List/Maximum and Minimum.py
@@ -0,0 +1,12 @@
+# Maximum and Minimum
+# 19Q
+n=int(input())
+a=[]
+for i in range(n):
+    a.append(int(input()))
+k=int(input())
+for i in range(0,n-1):
+    for j in range(0,n-1):
+        if a[j]>a[j+1]:
+            a[j],a[j+1]=a[j+1],a[j]
+print(a[n-k])
