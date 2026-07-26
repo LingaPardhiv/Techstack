ad12f9e8b36bd291a96c6e517509feb274df7aa3
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-07-26 23:11:01 +0530

    feat: add array operations to array-2D file

--- python/Data Analytics/array-2D.py
+++ python/Data Analytics/array-2D.py
@@ -51,3 +51,98 @@ print(n.divide(arr3,arr4))
 print(n.sqrt(arr3))
 print(n.sqrt(arr4))
 print(n.pow(arr4,2))
+
+#concatenation
+l5 = [[32,43,34,32,43,45],[23,54,23,65,43,67]]
+l6 = [[43,56,87,23,65,65],[34,65,76,54,43,24]]
+arr5=n.array(l5)
+print("arr5 ->",arr5)
+arr6=n.array(l6)
+print("arr5 ->",arr5)
+print(arr5+arr6)
+print(n.concatenate([arr5,arr6]))
+#vertical concatenation
+print(n.concatenate([arr5,arr6], axis=0))
+print("vstack -> \n",n.vstack([arr5,arr6]))
+#horizontal concatenation
+print(n.concatenate([arr5,arr6], axis=1))
+print("hstack -> \n",n.hstack([arr5,arr6]))
+
+#appending
+print('arr5 -> ',arr5,'arr6 -> ',arr6)
+print(n.append(arr5,90))
+print(n.append(arr6,[34,54,56]))
+print(n.append(arr5,arr6))
+
+#insert
+print('arr5 -> ',arr5,'arr6 -> ',arr6)
+print(n.insert(arr6,4,90))
+#vertical insertion
+print(n.insert(arr6,1,67,axis=0))
+print(n.insert(arr6,[0,1],[1,2,3,4,5,6],axis=0))
+#horizontal insertion
+print(n.insert(arr6,4,78,axis=1))
+
+#deletion
+print('arr5 -> \n',arr5,'\n arr6 -> \n',arr6)
+print(n.delete(arr6,3))
+#rows removal
+print(n.delete(arr6,1,axis=0))
+#column removal
+print(n.delete(arr6,3,axis=1))
+
+#sorting
+print(n.sort(arr6))
+
+#searching
+print(n.where(arr6==56))
+print(n.where(arr6==43))
+print("arr6 -> \n",arr6)
+print(n.where(arr6%2==0))
+
+l1 = [73,54,323,56,23,45]
+print("li ->",l1)
+arr1=n.array(l1)
+print('arr1 ->',arr1)
+
+#filteration
+print("arr1 -> \n",arr1)
+na=n.array([False,True,False,True,True,False])
+print('na -> \n',na)
+print(arr1[na])
+print(arr1[arr1>=100])
+
+# Aggregate Functions
+print('arr6 ->',arr6)
+print(n.sum(arr6))
+print(n.mean(arr6))
+print(n.max(arr6))
+print(n.min(arr6))
+print(n.size(arr6))
+print(n.cumsum(9))
+print(n.cumprod(6))
+
+price = [50,100,150,200,250]
+print("Price -> \n", price)
+products_Quantity = n.array([2,6,3,4,1])
+print("products_Quantity -> \n",products_Quantity)
+
+customer = n.array([[50,100,150,200,250],[2,6,3,4,1]])
+print("Customer -> \n",customer)
+print(n.cumprod(customer))
+items_total = n.cumprod(customer, axis = 0)
+print("items_total -> \n",items_total)
+total_bill = n.sum(items_total[1])
+print("total_bill -> \n",total_bill)
+
+# statistical functions
+print("arr6 -> \n",arr6)
+print(n.mean(arr6))
+print(n.median(arr6))
+
+import statistics as s
+print(arr6.flatten())
+print(s.mode(arr6.flatten()))
+
+print(n.std(arr6))
+print(n.var(arr6))
